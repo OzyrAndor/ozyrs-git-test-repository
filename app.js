6f75170e2ee8a5928ca6463299e49a4b32ce547b
Oskari Anttonen <anttoneno@gmail.com>
2021-12-10 21:13:44 +0200

    add basic javascript hide show -functionality

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ const selectorButton = document.querySelectorAll('.selector');
 function click(button) {
 
 }
+
+const targetDiv = document.querySelector('.showable');
+const btn = document.querySelector('.opener');
+btn.onclick = function() {
+    if (targetDiv.style.display !== "none") {
+        targetDiv.style.display = "none";
+    } else {
+        targetDiv.style.display = "block"
+    }
+
+};
